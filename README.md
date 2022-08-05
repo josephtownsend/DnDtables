48b962d000fbd5153105af3c2803f92d02f24331
Joseph Townsend <josephe.townsend@gmail.com>
2022-08-04 23:18:48 -0700

    Figured out plan for treasure tables

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DnDtables
+This repository is composed of code I used to randomly generate various tables, encounters, and loot for a DnD campaign I am running.
+
+Fundamentally, it is a personal project that I used to help learn various aspects of python.
---
+++ treasure.py
@@ -0,0 +1,12 @@
+# Roll to see how much treasure
+
+#def howMuchTreasure():
+import d20
+roll = d20.roll("1d20")
+print (roll.total)
+
+#Roll to see how many types of objects
+
+# Roll to see which objects, do as many times as required to reach the number of types of objects
+
+# loop through list, adding value of thing until get to amount of treasure required.
